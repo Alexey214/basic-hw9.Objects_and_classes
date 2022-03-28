2fb835252ab220800c9223d314741c294b4ccb84
brain <golenev214@gmail.com>
2022-03-28 18:49:05 +0300

    DZ9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Autor autor1 = new Autor("Антон", "Чехов");
+        Autor autor2 = new Autor("Федор", "Достаевский");
+        Book book1 = new Book("Чайка", autor1, 1998);
+        Book book2 = new Book("Преступление и наказание", autor2, 2006);
+        book1.setPublicationAge(2000);
+
+        autor1.printingAutorInfo(autor1);
+        autor2.printingAutorInfo(autor2);
+        book1.printingBookInfo(book1);
+        book2.printingBookInfo(book2);
     }
 }
